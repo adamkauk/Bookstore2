b49f292b45514219431b3ff9068ac17dadf0cec5
Adam K <kaukadam@yahoo.com>
2018-10-25 08:39:56 +0100

    removed class files, updated .gitignore, removed guts from InputOutput class

--- InputOutput.java
+++ InputOutput.java
@@ -12,63 +12,12 @@ import java.util.Scanner;
  */
 public abstract class InputOutput
 {
-    //The methods used here can be found in:
-    //The IB Computer Science subject guide (first examination in 2010)
-    //Appendix A: Core Computer Science for the IB Diploma Program (International Baccalaureate). Published by Express Publishing.
-	private static Scanner my_reader;
+
+	public static void output(String string) {
+		// TODO Auto-generated method stub
+		System.out.print(string);
+		
+	}
 	
-	public static void output(String str) 
-	{
-		System.out.println(str);
-	}
-
-	public static char inputChar(String string) {
-		output(string);
-		//BufferedReader input = new BufferedReader(input_reader);
-
-		if (InputOutput.my_reader == null) {
-			my_reader = new Scanner(System.in);
-		}
-			
-		char c = my_reader.next().charAt(0);
-		//reader.close();
-		return c;
-	}
-
-	public static String input(String string) {
-		output(string);
-		//Scanner reader = new Scanner(System.in);
-		if (InputOutput.my_reader == null) {
-			my_reader = new Scanner(System.in);
-		}
-
-		String c = my_reader.next();
-		//reader.forEachRemaining(arg0);
-		//reader.close();
-		return c;
-	}
-
-	public static int inputInt(String string) {
-		output(string);
-		//Scanner reader = new Scanner(System.in);
-		if (InputOutput.my_reader == null) {
-			my_reader = new Scanner(System.in);
-		}
-
-		String c = my_reader.next();
-		int i = Integer.parseInt(c);
-		//reader.close();
-		return i;
-	}
-
-	public static String inputString(String string) {
-		output(string);
-		//Scanner reader = new Scanner(System.in);
-		if (InputOutput.my_reader == null) {
-			my_reader = new Scanner(System.in);
-		}
-		String c = my_reader.next();
-		//reader.close();
-		return c;
-	}
+	
 }
