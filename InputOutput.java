6e7c540ca517aba20594350a4e65548ff30e78db
adamkauk <adamkauk@users.noreply.github.com>
2018-10-31 14:43:57 +0800

    Update InputOutput.java
    adding methods to InputOutput

--- InputOutput.java
+++ InputOutput.java
@@ -10,8 +10,11 @@ import java.util.Scanner;
  * @authors Kostas Dimitriou & Markos Hatzitaskos 
  * @version 1.0
  */
+
 public abstract class InputOutput
 {
+	
+	static Scanner my_scanner;
 
 	public static void output(String string) {
 		// TODO Auto-generated method stub
@@ -19,6 +22,28 @@ public abstract class InputOutput
 		System.out.println("hi");
 		return;
 	}
-	
+
+	public static String inputString(String string) {
+		if (InputOutput.my_scanner == null) {
+			my_scanner = new Scanner(System.in);
+		}
+		String s = my_scanner.next();
+		return s;
+	}
+
+	public static int inputInt(String string) {
+		String s = inputString(string);
+		int n = Integer.parseInt(s);
+		return n;
+	}
+
+	public static char inputChar(String string) {
+		String s = inputString(string);
+		return s.charAt(0);
+	}
+
+	public static String input(String string) {
+		return inputString(string);
+	}
 	
 }
