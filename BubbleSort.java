21419dc912c23081c4a0353141bf391eca8bd009
emma-2000 <43661883+emma-2000@users.noreply.github.com>
2018-10-31 15:36:57 +0800

    Inventory

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,75 +1,44 @@
 
-
 /**
- * BubbleSort class contains methods to sort various data structures in specific
- * orders using the bubble sort algorithm.
- *
+ * BubbleSort class contains methods to sort various data structures in specific orders using the
+ * bubble sort algorithm.
+ * 
  * @author Kostas Dimitriou & Markos Hatzitaskos
  * @version 1.0
  */
-public class BubbleSort {
-
+public class BubbleSort
+{
     /**
      * bubbleSortA - sorts the books in the bookstore by price.
-     *
-     * @param books an array containing the books to be sorted
+     * 
+     * @param books   an array containing the books to be sorted 
      */
-    public static Book[] bubbleSortA(Book[] books, boolean ascending) {
+    public static Book[] bubbleSortA(Book[] books, boolean ascending)
+    {
         int size = books.length;
         int last, current;
         Book temp;
-        for (last = size - 1; last > 0; last = last - 1) {
-            for (current = 0; current < last; current = current + 1) {
-                if (ascending) {
-                    if (books[current].getPrice() > books[current + 1].getPrice()) {
+        for(last = size-1; last > 0; last = last - 1)
+        { for(current = 0; current < last; current = current + 1)
+            { 
+                if(ascending) {
+                    if (books[current].getPrice() > books[current + 1].getPrice())
+                    {
                         temp = books[current];
-                        books[current] = books[current + 1];
-                        books[current + 1] = temp;
+                        books[current] = books[current+1];
+                        books[current+1] = temp;
                     }
                 } else {
-                    if (books[current].getPrice() < books[current + 1].getPrice()) {
+                    if (books[current].getPrice() < books[current + 1].getPrice())
+                    {
                         temp = books[current];
-                        books[current] = books[current + 1];
-                        books[current + 1] = temp;
+                        books[current] = books[current+1];
+                        books[current+1] = temp;
                     }
                 }
             }
         }
-        return books;
-    }
-
-    public static Book[] bubbleSortB(Book[] books) {
-        int size = books.length;
-        int last, current;
-        Book temp;
-        for (last = size - 1; last > 0; last = last - 1) {
-            for (current = 0; current < last; current = current + 1) {
-                
-                if ((books[current+1].getAuthor()).compareToIgnoreCase(books[current].getAuthor()) < 0) {
-                    temp = books[current];
-                    books[current] = books[current + 1];
-                    books[current + 1] = temp;
-                }
-            }
-        }
-        return books;
-    }
-    
-    
-    public static Book[] bubbleSortC(Book[] books) {
-        int size = books.length;
-        int last, current;
-        Book temp;
-        for (last = size - 1; last > 0; last = last - 1) {
-            for (current = 0; current < last; current = current + 1) {
-                
-                if ((books[current+1].getTitle()).compareToIgnoreCase(books[current].getTitle()) < 0) {
-                    temp = books[current];
-                    books[current] = books[current + 1];
-                    books[current + 1] = temp;
-                }
-            }
-        }
+        
         return books;
     }
 }
--- InputTerminal.java
+++ InputTerminal.java
@@ -1,75 +1,82 @@
 
 /**
- * class InputTerminal Allows the user to add a book or a magazine to the
- * Bookstore through the terminal.
- *
+ * class InputTerminal
+ * Allows the user to add a book or a magazine to the Bookstore through the terminal.
+ * 
  * @author Kostas Dimitriou & Markos Hatzitaskos
  * @version 1.0
  */
-public class InputTerminal {
+public class InputTerminal
+{
+    // instance variables
+    private String title, author;
+    private int id, pages, price, chapters, inventory;
+    //private String[] authorList;
+ 
+    /**
+     * bookEntry method
+     * 
+     * Responsible for getting user input for a new book entry in the bookstore.
+     */
+    public Book bookEntry()
+    {      
+        InputOutput.output("");
+        InputOutput.output("");
+        InputOutput.output("===================");
+        InputOutput.output("New bookstore entry");
+        InputOutput.output("===================");
+        InputOutput.output("");
+        InputOutput.output("");
+        InputOutput.output("");
+        InputOutput.output("Book details");
+        InputOutput.output("------------");
+        InputOutput.output("");
 
-	// instance variables
-	private String title, author;
-	private int id, pages, price, chapters;
-	private String[] authorList;
+        id = InputOutput.inputInt("id: ");
+        while(id < 0 || id == 0) {
+            InputOutput.output("Error: The id you entered was not a proper id number");
+            id = InputOutput.inputInt("id: ");
+        } 
 
-	/**
-	 * bookEntry method
-	 *
-	 * Responsible for getting user input for a new book entry in the bookstore.
-	 */
-	public Book bookEntry() {
-		InputOutput.output("");
-		InputOutput.output("");
-		InputOutput.output("===================");
-		InputOutput.output("New bookstore entry");
-		InputOutput.output("===================");
-		InputOutput.output("");
-		InputOutput.output("");
-		InputOutput.output("");
-		InputOutput.output("Book details");
-		InputOutput.output("------------");
-		InputOutput.output("");
+        title = InputOutput.inputString("title: ");
+        while(title.trim().length() == 0 || title.trim().length() > 25) {
+            InputOutput.output("Error: The title you entered was empty or has more than 25 characters");
+            title = InputOutput.inputString("title: ");
+        } 
 
-		id = InputOutput.inputInt("id: ");
-		while (id < 0 || id == 0) {
-			InputOutput.output("Error: The id you entered was not a proper id number");
-			id = InputOutput.inputInt("id: ");
-		}
+        pages = InputOutput.inputInt("pages: ");
+        while(pages < 0 || pages == 0) {
+            InputOutput.output("Error: The pages you entered was not a positive number");
+            pages = InputOutput.inputInt("pages: ");
+        } 
 
-		title = InputOutput.inputString("title: ");
-		while (title.trim().length() == 0 || title.trim().length() > 25) {
-			InputOutput.output("Error: The title you entered was empty or has more than 25 characters");
-			title = InputOutput.inputString("title: ");
-		}
+        price = InputOutput.inputInt("price ($): ");
+        while(price < 0 || price == 0) {
+            InputOutput.output("Error: The price you entered was not a positive number");
+            price = InputOutput.inputInt("price: ($)");
+        } 
 
-		pages = InputOutput.inputInt("pages: ");
-		while (pages < 0 || pages == 0) {
-			InputOutput.output("Error: The pages you entered was not a positive number");
-			pages = InputOutput.inputInt("pages: ");
-		}
+        chapters = InputOutput.inputInt("chapters: ");
+        while(chapters < 0 || chapters == 0) {
+            InputOutput.output("Error: The chapters you entered was not a positive number");
+            chapters = InputOutput.inputInt("chapters: ");
+        }
+        
+        author = InputOutput.inputString("author: ");
 
-		price = InputOutput.inputInt("price ($): ");
-		while (price < 0 || price == 0) {
-			InputOutput.output("Error: The price you entered was not a positive number");
-			price = InputOutput.inputInt("price: ($)");
-		}
+        while(author.trim().length() == 0  || title.trim().length() > 25) {
+                InputOutput.output("Error: The author name you entered was empty or has more than 25 characters");
+                author = InputOutput.inputString("author: ");
+        }
 
-		chapters = InputOutput.inputInt("chapters: ");
-		while (chapters < 0 || chapters == 0) {
-			InputOutput.output("Error: The chapters you entered was not a positive number");
-			chapters = InputOutput.inputInt("chapters: ");
-		}
+        inventory = InputOutput.inputInt("inventory: ");
+        while(inventory < 0 || inventory == 0) {
+            InputOutput.output("Error: The number you entered was not a positive number");
+            price = InputOutput.inputInt("inventory: ");
+        }
 
-		author = InputOutput.inputString("author: ");
-
-		while (author.trim().length() == 0 || title.trim().length() > 25) {
-			InputOutput.output("Error: The author name you entered was empty or has more than 25 characters");
-			author = InputOutput.inputString("author: ");
-		}
-
-		Book b = new Book(id, title, pages, price, chapters, author);
-
-		return b;
-	}
+        Book b = new Book(id, title, pages, price, chapters, author, inventory);
+        
+        return b;
+    }
 }
