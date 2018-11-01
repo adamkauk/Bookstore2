603f9e6a01c0c255165260c4522997c54a1bb965
emma-2000 <43661883+emma-2000@users.noreply.github.com>
2018-11-01 08:46:55 +0800

    Emma Kevin Combined

--- Bookstore.java
+++ Bookstore.java
@@ -1,101 +1,99 @@
-
-
 /**
- * Bookstore class is the main class for the Bookstore example.
- *
- * @author Kostas Dimitriou & Markos Hatzitaskos
+ * Bookstore class is the main class for the Bookstore example. 
+ * 
+ * @author Kostas Dimitriou & Markos Hatzitaskos 
  * @version 1.0
  */
-public class Bookstore {
-
+public class Bookstore
+{
     // instance variables
     BookFile bf;
-
+    
     /**
      * Main method
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         new Bookstore();
     }
-
+    
     /**
      * Constructor for objects of class Bookstore
      */
-    public Bookstore() {
+    public Bookstore() 
+    {      
         //sequentially
         bf = new BookFile();
-
+ 
         showMenu();
     }
 
     /**
      * showMenu method - displays the start menu in the Terminal.
-     *
+     * 
      */
-    public void showMenu() {
+    public void showMenu()
+    {
         InputOutput.output("========================");
         InputOutput.output("Welcome to the Bookstore");
         InputOutput.output("========================");
         InputOutput.output("");
         InputOutput.output("Select one of the following actions:");
         InputOutput.output("a) List all the books in the bookstore");
-        InputOutput.output("b) Add a book to the boostore");
+        InputOutput.output("b) Add a book to the bookstore");
         InputOutput.output("c) Sort books by price (descending)");
         InputOutput.output("d) Sort books by price (ascending)");
         InputOutput.output("e) Find book with title");
         InputOutput.output("f) Sort books by author name");
         InputOutput.output("g) Sort books by Title");
-        InputOutput.output("h) Exit");
+        InputOutput.output("h) Change inventory");
+        InputOutput.output("i) Exit");
         InputOutput.output("");
         InputOutput.output("");
 
         char selection = InputOutput.inputChar("Which action do you want to perform? ");
-
+        
         switch (selection) {
-            case 'a':
-                listAllBooks();
-                break;
-            case 'b':
-                addBook();
-                break;
-            case 'c':
-                sortByPrice(false);
-                break;
-            case 'd':
-                sortByPrice(true);
-                break;
-            case 'e':
-                findBookTitle();
-                break;
+            case 'a' : listAllBooks();
+            break; 
+            case 'b' : addBook();   
+            break;
+            case 'c' : sortByPrice(false);
+            break;
+            case 'd' : sortByPrice(true);
+            break;
+            case 'e' : findBookTitle();
+            break;
             case 'f':
                 sortByauthor();
             case 'g':
                 sortByTitle();
-            case 'h':
-                System.exit(0);
-                break;
-            default:
-                break;
+            case 'h' : changeInventory();
+            case 'i' : System.exit(0);
+            break;
+            default : showMenu();
+            break;
         }
     }
 
     /**
-     * listAllBooks method - lists all the books of the bookstore in the
-     * terminal.
+     * listAllBooks method - lists all the books of the bookstore in the terminal. 
      */
-    public void listAllBooks() {
-        //sequentially
-        bf.outputAllBooksToTerminal();
-        InputOutput.output("");
-        InputOutput.input("Press ANY BUTTON to continue.");
-        InputOutput.output("");
-        showMenu();
+    public void listAllBooks()
+    {
+            //sequentially
+            bf.outputAllBooksToTerminal();
+            InputOutput.output("");
+            InputOutput.input("Press ANY BUTTON to continue.");
+            InputOutput.output("");
+            showMenu();
     }
 
     /**
      * addBook method - allows the user to add a book through the terminal
      */
-    public void addBook() {
+    public void addBook()
+    {
         InputTerminal addBook = new InputTerminal();
         Book b = addBook.bookEntry();
 
@@ -107,23 +105,38 @@ public class Bookstore {
         showMenu();
     }
 
+    public void changeInventory(){
+        String searchParameter = InputOutput.inputString("Please enter the title of the book:");
+        int newInventory = InputOutput.inputInt("Please enter the new inventory:");
+        Book[] books = null;
+        books = bf.readBookFile();
+        int toShow = SequentialSearch.sequentialSearchTitle(searchParameter, books);
+        books[toShow].setInventory(newInventory);
+        InputOutput.output("You have successfully changed the inventory!");
+        InputOutput.output("The new inventory for " + books[toShow].getTitle() + "is " + books[toShow].getInventory());
+        InputOutput.output("");
+        InputOutput.input("Press ANY BUTTON to continue.");
+        InputOutput.output("");
+        showMenu();
+    }
+
     /**
      * sortByPrice method
-     *
-     * @param ascending whether the Bookstore list is sorted in an ascending or
-     * descending way.
+     * 
+     * @param ascending     whether the Bookstore list is sorted in an ascending or descending way. 
      */
-    public void sortByPrice(boolean ascending) {
+    public void sortByPrice(boolean ascending)
+    {
 
         Book[] books = null;
 
         //sequentially
         books = bf.readBookFile();
 
-        if (books == null || books.length == 0) {
+        if(books == null || books.length == 0) 
             InputOutput.output("There are no books in the bookstore to sort.");
-        } else {
-            if (ascending) {
+        else {
+            if(ascending) {
                 books = BubbleSort.bubbleSortA(books, true);
                 InputOutput.output("");
                 InputOutput.output("Sorted books by price (ascending):");
@@ -135,13 +148,14 @@ public class Bookstore {
                 InputOutput.output("");
             }
 
-            for (int i = books.length - 1; i >= 0; i--) {
+            for(int i=books.length-1; i>=0; i--) {
                 int id = books[i].getId();
                 String title = books[i].getTitle();
                 int pages = books[i].getPages();
                 int price = books[i].getPrice();
                 int chapters = books[i].getChapters();
                 String author = books[i].getAuthor();
+                int inventory = books[i].getInventory();
 
                 InputOutput.output("");
                 InputOutput.output("=================================");
@@ -151,6 +165,7 @@ public class Bookstore {
                 InputOutput.output("PRICE ($):" + price);
                 InputOutput.output("CHAPTERS:" + chapters);
                 InputOutput.output("AUTHOR(S):" + author);
+                InputOutput.output("INVENTORY" + inventory);
 
                 InputOutput.output("=================================");
                 InputOutput.output("");
@@ -165,10 +180,11 @@ public class Bookstore {
 
     /**
      * findBookTitle method
-     *
-     * @return find a Book with a given book title.
+     * 
+     * @return     find a Book with a given book title.
      */
-    public void findBookTitle() {
+    public void findBookTitle()
+    {
         InputOutput.output("");
         InputOutput.output("Search for a book.");
         String searchParameter = InputOutput.input("Title to search: ");
@@ -183,16 +199,17 @@ public class Bookstore {
 
         do {
 
-            if (toShow == -1) {
+            if(toShow == -1) 
                 InputOutput.output("There is no book in the bookstore with such a title.");
-            } else {
+            else {
                 int id = books[toShow].getId();
                 String title = books[toShow].getTitle();
                 int pages = books[toShow].getPages();
                 int price = books[toShow].getPrice();
                 int chapters = books[toShow].getChapters();
                 String author = books[toShow].getAuthor();
-
+                int inventory = books[toShow].getInventory();
+                
                 InputOutput.output("");
                 InputOutput.output("=================================");
                 InputOutput.output("ID:" + id);
@@ -201,33 +218,32 @@ public class Bookstore {
                 InputOutput.output("PRICE ($):" + price);
                 InputOutput.output("CHAPTERS:" + chapters);
                 InputOutput.output("AUTHOR(S):" + author);
+                InputOutput.output("INVENTORY" + inventory);
                 InputOutput.output("=================================");
                 InputOutput.output("");
 
                 int ltemp = books.length - toShow - 1;
                 Book[] temp = new Book[ltemp];
 
-                for (int i = 0; i < temp.length; i++) {
-                    temp[i] = books[toShow + i + 1];
-                }
+                for(int i=0; i<temp.length; i++)
+                    temp[i] = books[toShow+i+1];
 
                 books = temp;
 
                 toShow = SequentialSearch.sequentialSearchTitle(searchParameter, books);
 
             }
-        } while (toShow != -1);
+        } while(toShow != -1);
 
         InputOutput.output("");
         InputOutput.input("Press ANY BUTTON to continue.");
         InputOutput.output("");
         showMenu();
     }
-
     public void sortByauthor() {
         Book[] books = null;
 
-        
+
         books = bf.readBookFile();
 
         if (books == null || books.length == 0) {
@@ -266,12 +282,12 @@ public class Bookstore {
         showMenu();
 
     }
-    
-    
+
+
     public void sortByTitle() {
         Book[] books = null;
 
-        
+
         books = bf.readBookFile();
 
         if (books == null || books.length == 0) {
--- ReadingMaterial.java
+++ ReadingMaterial.java
@@ -1,140 +1,143 @@
 
 /**
- * Abstract class ReadingMaterial This class describes the basic information of
- * a reading material in the bookstore. It is an abstract method that can be
- * extended to describe a more concrete type of reading material such as a book
- * or a magazine.
+ * Abstract class ReadingMaterial
+ * This class describes the basic information of a reading material in the
+ * bookstore. It is an abstract method that can be extended to describe a
+ * more concrete type of reading material such as a book or a magazine.
  * 
  * @author Kostas Dimitriou & Markos Hatzitaskos
  * @version 1.0
  */
-public class ReadingMaterial {
-	protected static String bookstoreName;
+public class ReadingMaterial
+{
+    protected static String bookstoreName;
+    
+    // Instance variables
+    private int id;
+    private String title;
+    private int pages;
+    private int price;
+    private int inventory;
+    
+    /**
+     * Constructor
+     * 
+     * @param  id    the id of the ReadingMaterial 
+     * @param  title    the title of the ReadingMaterial
+     * @param  pages    the number of pages of the ReadingMaterial
+     * @param  price    the price of the ReadingMaterial
+     */
+    ReadingMaterial(int id, String title, int pages, int price, int inventory)
+    {
+        bookstoreName = "K&M Bookstore";
+        
+        this.id = id;
+        this.inventory = inventory;
+        this.title = title;
+        this.pages = pages;
+        this.price = price;
+    }
+    
+    /**
+     * Accessor method
+     * 
+     * @return        the name of the Bookstore 
+     */
+    public String getBookstoreName()
+    { return bookstoreName; }
+    
+    /**
+     * Mutator method
+     * 
+     * @param  bookstoreName   the name of the Bookstore
+     */
+    public void setBookstoreName(String bookstoreName)
+    { ReadingMaterial.bookstoreName = bookstoreName; }
+    
+    /**
+     * Accessor method
+     * 
+     * @return        the id of the ReadingMaterial 
+     */
+    public int getId()
+    { return id; }
+    
+    /**
+     * Mutator method
+     * 
+     * @param  id    the id of the ReadingMaterial 
+     */
+    public void setId(int id)
+    { this.id = id; }
+    
+    /**
+     * Accessor method
+     * 
+     * @return        the title of the ReadingMaterial 
+     */
+    public String getTitle()
+    { return title; }
+    
+    /**
+     * Mutator method
+     * 
+     * @param  title    the title of the ReadingMaterial 
+     */
+    public void setTitle(String title)
+    { this.title = title; }
+    
+    /**
+     * Accessor method
+     * 
+     * @return        the number of pages of the ReadingMaterial 
+     */
+    public int getPages()
+    { return pages; }
+    
+    /**
+     * Mutator method
+     * 
+     * @param  pages    the number of pages of the ReadingMaterial 
+     */
+    public void setPages(int pages)
+    { this.pages = pages; }
+    
+    /**
+     * Accessor method
+     * 
+     * @return        the price of the Reading material 
+     */
+    public int getPrice()
+    { return price; }
+    
+    /**
+     * Mutator method
+     * 
+     * @param  price    the price of the ReadingMaterial 
+     */
+    public void setPrice(int price)
+    { this.price = price; }
+    
+    /**
+     * Mutator method
+     * 
+     * @param  increase    increase the price of the ReadingMaterial by a number
+     */
+    public void changePriceBy(int increase)
+    { 
+        int newPrice = price + increase;
 
-	// Instance variables
-	private int id;
-	private String title;
-	private int pages;
-	private int price;
+        if(newPrice >= 0) {
+                	this.price = newPrice; 
+        }
+    }
 
-	/**
-	 * Constructor
-	 * 
-	 * @param id    the id of the ReadingMaterial
-	 * @param title the title of the ReadingMaterial
-	 * @param pages the number of pages of the ReadingMaterial
-	 * @param price the price of the ReadingMaterial
-	 */
-	ReadingMaterial(int id, String title, int pages, int price) {
-		bookstoreName = "K&M Bookstore";
+    public int getInventory(){
+        return inventory;
+    }
 
-		this.id = id;
-		this.title = title;
-		this.pages = pages;
-		this.price = price;
-	}
-
-	/**
-	 * Accessor method
-	 * 
-	 * @return the name of the Bookstore
-	 */
-	public String getBookstoreName() {
-		return bookstoreName;
-	}
-
-	/**
-	 * Mutator method
-	 * 
-	 * @param name the name of the Bookstore
-	 */
-	public void setBookstoreName(String bookstoreName) {
-		this.bookstoreName = bookstoreName;
-	}
-
-	/**
-	 * Accessor method
-	 * 
-	 * @return the id of the ReadingMaterial
-	 */
-	public int getId() {
-		return id;
-	}
-
-	/**
-	 * Mutator method
-	 * 
-	 * @param id the id of the ReadingMaterial
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * Accessor method
-	 * 
-	 * @return the title of the ReadingMaterial
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * Mutator method
-	 * 
-	 * @param title the title of the ReadingMaterial
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/**
-	 * Accessor method
-	 * 
-	 * @return the number of pages of the ReadingMaterial
-	 */
-	public int getPages() {
-		return pages;
-	}
-
-	/**
-	 * Mutator method
-	 * 
-	 * @param pages the number of pages of the ReadingMaterial
-	 */
-	public void setPages(int pages) {
-		this.pages = pages;
-	}
-
-	/**
-	 * Accessor method
-	 * 
-	 * @return the price of the Reading material
-	 */
-	public int getPrice() {
-		return price;
-	}
-
-	/**
-	 * Mutator method
-	 * 
-	 * @param price the price of the ReadingMaterial
-	 */
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	/**
-	 * Mutator method
-	 * 
-	 * @param increase increase the price of the ReadingMaterial by a number
-	 */
-	public void changePriceBy(int increase) {
-		int newPrice = price + increase;
-
-		if (newPrice >= 0) {
-			this.price = newPrice;
-		}
-	}
+    public void setInventory (int inventory){
+        this.inventory = inventory;
+    }
 
 }
