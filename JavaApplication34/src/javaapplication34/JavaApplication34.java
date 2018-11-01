382ebe8b6758fa1d7e0a184680f085e6c7b4feb7
Adam K <kaukadam@yahoo.com>
2018-11-01 01:18:43 +0100

    changed package name in JavaApplication34

--- JavaApplication34/src/javaapplication34/JavaApplication34.java
+++ JavaApplication34/src/javaapplication34/JavaApplication34.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication34;
+package JavaApplication34.src.javaapplication34;
 
 /**
  *
